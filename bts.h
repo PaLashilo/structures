c8de080a950ec90d74f0f629a2050d6c2a7fcbc8
PaLashilo <93659071+PaLashilo@users.noreply.github.com>
2023-08-22 22:14:26 +0300

    binary tree

--- bts.h
+++ bts.h
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct s_btree 
-{
-    struct s_btree *left; 
+typedef struct s_btree {
+    struct s_btree *left;
     struct s_btree *right;
-    int item; 
+    int item;
 } t_btree;
-
-
-
